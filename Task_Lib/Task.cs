316cc6b2948b2ccf82acfa829181c076d00dea84
lilyakaras <lilyca.hrupa@gmail.com>
2020-11-19 09:38:36 +0200

    Add project files.

---
+++ Task_Lib/Task.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Lib
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public State state { get; set; }
+    }
+
+    public enum State
+    {
+        Open,
+        InProgress,
+        Close
+    }
+    public class Tasks
+    {
+        List<Task> tasks = new List<Task>();
+        public void AddTask(Task t)
+        {
+
+        }
+    }
+}
